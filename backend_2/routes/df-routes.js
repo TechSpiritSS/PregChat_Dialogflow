5e156e03653ad745e1279b40b0e30a494f889e48
Sidharth Sethi <76687985+TechSpiritSS@users.noreply.github.com>
2023-04-04 04:22:05 +0530

    added

---
+++ backend_2/routes/df-routes.js
@@ -0,0 +1,13 @@
+const chatbot = require('../chatbot/chatbot');
+
+module.exports = function (app) {
+  app.post('/text_query', async (req, res) => {
+    // console.log(req);
+
+    const { text, userId } = req.body;
+    const resultQuery = await chatbot.textQuery(text, userId);
+    // console.log(resultQuery);
+    res.send(resultQuery.fulfillmentMessages[0].text.text[0]);
+    // res.send('This is a response from the Text Query route');
+  });
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+// import './index.css';
 
 // import Kommunicate from '@kommunicate/kommunicate-chatbot-plugin';
 
